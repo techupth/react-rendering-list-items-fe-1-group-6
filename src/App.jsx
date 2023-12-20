26f6f6627c9ac41092520c037ef447a0b3034009
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-12-20 16:36:11 +0700

    style : rendering list movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
+import movies from "./data/movies";
+
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <h1>Movie List Section</h1>
+        {
+        movies.map((movie , index) => {
+          return  <div key={index} className="movies-card">
+                    <div className="movies-img">
+                      <img src={movie.image} alt="movie image" width={102} height={100} />
+                    </div>
+                    <div className="movies-details">
+                      <h4>{movie.title}</h4>  
+                      <p>Year: {movie.year}</p>
+                      <p>Runtime: {movie.runtime} Mins</p>
+                      <p>Genres: {movie.genres.map((genres)=> <span>{genres}</span>)}</p>
+                      <p>IMDB Rating: {movie.imdbRating} </p>
+                      <p>IMDB Votes: {movie.imdbVotes}</p>
+                    </div>
+                  </div>
+        })
+        }
+
       </section>
     </div>
   );
